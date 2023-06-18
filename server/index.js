ce65904b12f92f00e9a0bf6b31071a7e77fa20d3
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-06-17 23:04:39 -0700

    User routes setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 
 /* CONFIGURATIONS */
@@ -42,13 +43,16 @@ app.post('/auth/register', upload.single('picture'), register);
 
 /* ROUTES */
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes); 
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-})
-.catch((error) => console.log(`${error} did not connect`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
